1accb389049abe7c2c7c83d2881b29699872152c
@hexlet/cli <support@hexlet.io>
2024-03-13 23:48:05 +0300

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,16 +1,13 @@
 package exercise.controller.users;
 
-import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import exercise.model.Post;
@@ -36,7 +33,12 @@ public class PostsController {
     @PostMapping("/users/{id}/posts")
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@PathVariable String id, String slug, String title, String body) {
-        posts.add(new Post(Integer.parseInt(id), slug, title, body));
+        var post = new Post();
+        post.setSlug(slug);
+        post.setTitle(title);
+        post.setBody(body);
+        post.setUserId(Integer.parseInt(id));
+        posts.add(post);
     }
 }
 // END
